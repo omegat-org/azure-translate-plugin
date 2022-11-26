90bcc2c4f99618017b1df2a247f2703e5e6f16e3
Hiroshi Miura <miurahr@linux.com>
2022-11-26 16:08:33 +0900

    Import OmegaT's Azure Translator V2 source

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,19 +11,23 @@ plugins {
 version = "0.0.1"
 
 omegat {
-    version = "5.4.1"
-    pluginClass = "org.omegat.filters2.text.dokuwiki.DokuWikiFilter"
+    version = "5.7.1"
+    pluginClass = "org.omegat.connectors.machinetranslators.MicrosoftTranslatorAzure"
 }
 
 dependencies {
-    packIntoJar("org.slf4j:slf4j-api:1.7.21")
-    implementation("commons-io:commons-io:2.5")
+    implementation("org.slf4j:slf4j-api:1.7.21")
+    implementation("commons-io:commons-io:2.11.0")
     implementation("commons-lang:commons-lang:2.6")
-    implementation("org.slf4j:slf4j-nop:1.7.21")
-    testImplementation("junit:junit:4.12")
-    testImplementation("xmlunit:xmlunit:1.6")
+    runtimeOnly("org.slf4j:slf4j-nop:1.7.21")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.xmlunit:xmlunit-legacy:2.8.3")
     testImplementation("org.madlonkay.supertmxmerge:supertmxmerge:2.0.1")
     testImplementation("com.alibaba:fastjson:1.2.17")
+}
+
+repositories {
+    mavenCentral()
 }
 
 checkstyle {

d8b0836fd8b24a75a37d013e332217f67500af71
Hiroshi Miura <miurahr@linux.com>
2023-06-26 12:19:01 +0900

    Update copyright header
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/test/java/org/omegat/connectors/machinetranslators/azure/TestMicrosoftTranslatorAzure.java
+++ src/test/java/org/omegat/connectors/machinetranslators/azure/TestMicrosoftTranslatorAzure.java
@@ -1,3 +1,30 @@
+/*
+ *  OmegaT - Computer Assisted Translation (CAT) tool
+ *           with fuzzy matching, translation memory, keyword search,
+ *           glossaries, and translation leveraging into updated projects.
+ *
+ *  Copyright (C) 2012 Alex Buloichik, Didier Briel
+ *                2016-2017 Aaron Madlon-Kay
+ *                2018 Didier Briel
+ *                2022,2023 Hiroshi Miura
+ *                Home page: https://www.omegat.org/
+ *                Support center: https://omegat.org/support
+ *
+ *  This file is part of OmegaT.
+ *
+ *  OmegaT is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  OmegaT is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.omegat.connectors.machinetranslators.azure;
 
 import org.omegat.util.Language;

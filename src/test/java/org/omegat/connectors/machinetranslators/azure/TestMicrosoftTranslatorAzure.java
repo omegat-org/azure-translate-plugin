c549965d2ba017dce09d57d6569cc44617143615
Hiroshi Miura <miurahr@linux.com>
2023-06-26 18:19:34 +0900

    feat(test): add V3 test
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/test/java/org/omegat/connectors/machinetranslators/azure/TestMicrosoftTranslatorAzure.java
+++ src/test/java/org/omegat/connectors/machinetranslators/azure/TestMicrosoftTranslatorAzure.java
@@ -55,6 +55,7 @@ public class TestMicrosoftTranslatorAzure {
 
     private static final String TOKEN_PATH = "/sts/v1.0/issueToken";
     private static final String V2_API_PATH = "/v2/http.svc/Translate";
+    private static final String V3_API_PATH = "/translate";
     private static final String key = "abcdefg";
 
     private File tmpDir;
@@ -130,4 +131,47 @@ public class TestMicrosoftTranslatorAzure {
         @Override
         protected void setKey(String val, boolean temporary) {}
     }
+
+    @Test
+    void testResponseV3(WireMockRuntimeInfo wireMockRuntimeInfo) throws Exception {
+        File prefsFile = new File(tmpDir, Preferences.FILE_PREFERENCES);
+        Preferences.IPreferences prefs = new PreferencesImpl(new PreferencesXML(null, prefsFile));
+        prefs.setPreference(MicrosoftTranslatorAzure.ALLOW_MICROSOFT_TRANSLATOR_AZURE, true);
+        prefs.setPreference(MicrosoftTranslatorAzure.PROPERTY_V2, false);
+        prefs.setPreference(MicrosoftTranslatorAzure.PROPERTY_NEURAL, false);
+        init(prefsFile.getAbsolutePath());
+
+        String text = "Buy tomorrow";
+        String translation = "Morgen kaufen gehen ein";
+
+        WireMock wireMock = wireMockRuntimeInfo.getWireMock();
+        wireMock.register(post(urlPathEqualTo(TOKEN_PATH))
+                .withHeader("Content-Type", equalTo("application/json"))
+                .withHeader("Accept", equalTo("application/jwt"))
+                .withHeader("Ocp-Apim-Subscription-Key", equalTo(key))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "text/plain")
+                        .withBody("PSEUDOTOKEN")));
+        Map<String, StringValuePattern> expectedParams = new HashMap<>();
+        expectedParams.put("Authorization", containing("PSEUDOTOKEN"));
+        expectedParams.put("api-version", equalTo("3.0"));
+        expectedParams.put("from", equalTo("en"));
+        expectedParams.put("to", equalTo("de"));
+        expectedParams.put("contentType", equalTo("application/json; charset=UTF-8"));
+        wireMock.register(get(urlPathEqualTo(V3_API_PATH))
+                .withQueryParams(expectedParams)
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("[{\"translations\": [ {\"text\": \"" + translation + "\"}]}]")));
+        int port = wireMockRuntimeInfo.getHttpPort();
+        MicrosoftTranslatorAzure azure = new MicrosoftTranslatorAzureMock();
+        AzureTranslatorV3 translator = new AzureTranslatorV3(azure);
+        translator.setTokenUrl(String.format("http://localhost:%d%s", port, TOKEN_PATH));
+        translator.setUrl(String.format("http://localhost:%d%s", port, V3_API_PATH));
+        String result = translator.translate(new Language("EN"), new Language("DE"), text);
+        Assertions.assertEquals(translation, result);
+    }
+
 }
